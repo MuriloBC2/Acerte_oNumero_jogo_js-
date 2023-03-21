bb31cf835338891d1d14f8cab6a0964eb033eb15
MuriloBC2 <murilobarrosdocarmo@gmai.com>
2023-03-21 10:26:42 -0300

    Arrumação do código

--- js.js
+++ js.js
@@ -2,9 +2,9 @@
 let AleatorioN = Math.floor(Math.random() * 100)+1
 
 //variaveis do resul
-const chances = document.querySelector('.chances')
-const ultresultado = document.querySelector('.ultresultado')
-const altobaixo = document.querySelector('.altobaixo')
+const chances = document.querySelector('#chances')
+const ultresultado = document.querySelector('#ultresultado')
+const altobaixo = document.querySelector('#altobaixo')
 
 //variaveis form
 const adcampo = document.querySelector('#adcampo')
@@ -18,26 +18,26 @@ let resetjogo
 function play(){
   const jogada = Number(adcampo.value)
 
-  if (contagem ===1 ){
-    chances.textContent = `Jogadas Anteriores: `
+  if (contagem == 1 ){
+    chances.innerHTML = `Jogadas Anteriores: `
   }
 
   chances.textContent = `${jogada}`
   
-  if (jogada === AleatorioN){
-    ultresultado.textContent = `Parabens, você acertou !!`
-    altobaixo.textContent = ``
+  if (jogada == AleatorioN){
+    ultresultado.innerHTML = `Parabens, você acertou !!`
+    altobaixo.innerHTML = ``
     ResetarJogo()
-  } else if (chances === 10) {
-    chances.textContent = `Game Over !!`
-    altobaixo.textContent = ''
+  } else if (chances == 10) {
+    chances.innerHTML = `Game Over !!`
+    altobaixo.innerHTML = ''
     ResetarJogo()
   } else {
-    ultresultado.textContent -'Errado!'
+    ultresultado.innerHTML -'Errado!'
     if(jogada < AleatorioN){
-       altobaixo.textContent ="Chute baixo"
+       altobaixo.innerHTML ="Chute baixo"
   } else if (jogada > AleatorioN){
-    altobaixo.textContent =' Chute alto demais. '
+    altobaixo.innerHTML =' Chute alto demais. '
   }
 }
        chances++
@@ -46,4 +46,3 @@ function play(){
 
 }
 
-confirmo.addEventListener('click' , play() )
